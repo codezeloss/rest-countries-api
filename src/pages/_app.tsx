193bc200efdd757461fa4f0f9360f61c33eea0c1
codezeloss <elhoussaineaitX@gmail.com>
2023-04-14 14:24:51 +0000

    fixing some UI bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app";
 import { createContext, useState } from "react";
 import "@/styles/globals.css";
 import Layout from "@/Components/Layout";
-
 export const ThemeContext = createContext(false);
 
 export default function App({ Component, pageProps }: AppProps) {
